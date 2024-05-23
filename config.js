6a053d9f7f83d95358ed7574d11af83a26f56aa5
Valentina <valentinavarg@gmail.com>
2024-05-23 13:41:14 -0500

    ajustes nuevos de bases de datos

--- config.js
+++
@@ -1,8 +0,0 @@
-
-exports.BD_HOST = process.env.BD_HOST || 'roundhouse.proxy.rlwy.net'
-exports.BD_USER = process.env.BD_USER || 'root'
-exports.BD_PASSWORD = process.env.BD_PASSWORD || 'dkcvUgZRKcVDddtZueDubXfQPZHEoCVU'
-exports.BD_NAME = process.env.BD_NAME || 'railway'
-exports.BD_PORT = process.env.BD_PORT || 23145
-
-
--- database/db.js
+++ database/db.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mysql = require('mysql')
-const DB = require('../config')
 
 const conexion = mysql.createConnection({
     multipleStatements: true,
-    host: DB.BD_HOST,
-    port: DB.BD_PORT,
-    user: DB.BD_USER,
-    password: DB.BD_PASSWORD,
-    database: DB.BD_NAME
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT,
+    user: process.env.BD_USER,
+    password: process.env.BD_PASSWORD,
+    database: process.env.BD_NAME
 })
 
 console.log(DB.BD_HOST, DB.BD_PORT, DB.BD_USER)
