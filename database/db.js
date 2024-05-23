2b69232c972b2c91fd5048035d541800994a3817
Valentina <valentinavarg@gmail.com>
2024-05-23 15:48:49 -0500

    ajustes nuevos de bases de datos

--- database/db.js
+++ database/db.js
@@ -1,16 +1,11 @@
 const express = require('express')
+require("dotenv").config();
 const mysql = require('mysql')
 
-const conexion = mysql.createConnection({
-    multipleStatements: true,
-    host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
-    user: process.env.BD_USER,
-    password: process.env.BD_PASSWORD,
-    database: process.env.BD_NAME
-})
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 
-console.log(process.env.BD_HOST, process.env.BD_PORT, process.env.BD_USER, process.env.BD_PASSWORD, process.env.BD_NAME)
+const conexion = mysql.createConnection(urlDB)
+
 
 conexion.connect((error)=>{
     if(error){
